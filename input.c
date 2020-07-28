9c4ded5f12a2f66d5046e30959704fe6d356c4ae
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-07-28 10:45:45 -0700

    Check It Out
    Check This Is The Best

---
+++ input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter The Value Of a : \n");
+    scanf("%d", &a);
+    printf("Enter The Value Of b \n");
+    scanf("%d", &b);
+    int c = a + b;
+    printf("Your Result Is  %d", a + b);
+    return 0;
+}
+
